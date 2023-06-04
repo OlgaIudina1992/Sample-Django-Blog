ffb04202c26daf2a98b8999c6b151ec0374db850
Olga Iudina <110724838+OlgaIudina1992@users.noreply.github.com>
2023-06-04 10:35:15 +0400

    Add RTF

--- myblog/pages/forms.py
+++ myblog/pages/forms.py
@@ -7,24 +7,25 @@ categories = [item for item in category_all]
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'category', 'body')
+        fields = ('title', 'title_tag', 'category', 'snippet', 'body')
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Please insert a title..."}),
             "title_tag": forms.TextInput(attrs={"class": "form-control"}),
             "category": forms.Select(choices=categories, attrs={"class": "form-control"}),
-            
             "body": forms.Textarea(attrs={"class": "form-control", "placeholder": "Type your post here..."}),
+            "snippet": forms.Textarea(attrs={"class": "form-control", "placeholder": "Your snippet here..."}),
 
         }
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'category', 'body')
+        fields = ('title', 'title_tag', 'category', 'body', 'snippet')
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Please insert a title..."}),
             "title_tag": forms.TextInput(attrs={"class": "form-control"}),
             "category": forms.Select(choices=categories, attrs={"class": "form-control"}),
             "body": forms.Textarea(attrs={"class": "form-control", "placeholder": "Type your post here..."}),
+            "snippet": forms.Textarea(attrs={"class": "form-control", "placeholder": "Your snippet here..."}),
 
         }
--- myblog/pages/models.py
+++ myblog/pages/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Category(models.Model):
@@ -23,7 +24,9 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default="New Post")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    #body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    snippet = models.CharField(max_length=500)
     pub_date = models.DateField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True)
 
